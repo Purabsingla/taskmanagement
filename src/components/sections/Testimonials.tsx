8c67edda491b56cc31f610384dbd0fe66e8cd234
Purabsingla <purabsingla15@gmail.com>
2025-02-17 19:21:21 +0530

    ..

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -32,7 +32,7 @@ const TestimonialSlider: React.FC = () => {
 
   return (
     <div>
-      <section className="bg-gray-50">
+      <section>
         <div className="mx-auto max-w-[1410px] px-4 py-12 sm:px-6 lg:me-0 lg:py-16 lg:ps-8 lg:pe-0 xl:py-24">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:items-center lg:gap-16">
             <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">

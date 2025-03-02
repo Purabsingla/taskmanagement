472d615988ea77ddeee48fa9f633fcc9afb7a983
Purabsingla <purabsingla15@gmail.com>
2025-03-02 16:40:20 +0530

    Refactor DialogFormBoard component for improved structure and readability

--- src/components/sections/DialogFormBoard.tsx
+++ src/components/sections/DialogFormBoard.tsx
@@ -20,31 +20,29 @@ const DialogBoard: React.FC<onClick> = ({ HandleClick }) => {
   };
 
   return (
-    <div>
-      <DialogContent className="sm:max-w-[650px] sm:max-h-auto">
-        <DialogHeader>
-          <DialogTitle>Create a New Board</DialogTitle>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Board Name
-            </Label>
-            <Input
-              id="name"
-              value={boardName}
-              onChange={(e) => setBoardName(e.target.value)}
-              className="col-span-3"
-            />
-          </div>
+    <DialogContent className="sm:max-w-[650px] sm:max-h-auto">
+      <DialogHeader>
+        <DialogTitle>Create a New Board</DialogTitle>
+      </DialogHeader>
+      <div className="grid gap-4 py-4">
+        <div className="grid grid-cols-4 items-center gap-4">
+          <Label htmlFor="name" className="text-right">
+            Board Name
+          </Label>
+          <Input
+            id="name"
+            value={boardName}
+            onChange={(e) => setBoardName(e.target.value)}
+            className="col-span-3"
+          />
         </div>
-        <DialogFooter>
-          <Button type="button" variant={"outline"} onClick={ReturnHandleClick}>
-            Save changes
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </div>
+      </div>
+      <DialogFooter>
+        <Button type="button" variant={"outline"} onClick={ReturnHandleClick}>
+          Save changes
+        </Button>
+      </DialogFooter>
+    </DialogContent>
   );
 };
 
